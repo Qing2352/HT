a93413d3796b0a59c11c10245e1c75d1374c0666
Your Name <2352607528@qq.com>
2020-08-23 22:19:06 +0800

    后台项目

---
+++ src/main/java/com/longyin/longyin/LongyinApplication.java
@@ -0,0 +1,13 @@
+package com.longyin.longyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongyinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongyinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longyin/longyin/LongyinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longyin.longyin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongyinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
